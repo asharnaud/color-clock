749f5f8620234ab503f6f94fa9cfa15d96b419f7
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-04-07 14:41:26 -0500

    Added hover to the time.

--- js/main.js
+++ js/main.js
@@ -13,13 +13,11 @@ currentTime()
 var everySecond = 1 * 1000
 var clock = setInterval(currentTime, everySecond)
 
-
-var theDate = new Date()
-var seconds = theDate.getSeconds()
-var hexSeconds = seconds.toString(16)
-var hex = '#' + hexSeconds + (Math.round((Math.random() * 50) + 1)) + (Math.round((Math.random() * 40) + 1))
-
 function changeTimeToHex () {
+  var theDate = new Date()
+  var seconds = theDate.getSeconds()
+  var hexSeconds = seconds.toString(16)
+  var hex = '#' + hexSeconds + (Math.round((Math.random() * 50) + 1)) + (Math.round((Math.random() * 40) + 1))
   console.log(hex)
   $('body').css('background', hex)
 }
@@ -27,17 +25,20 @@ changeTimeToHex()
 
 var changeColor = setInterval(changeTimeToHex, everySecond * 2)
 
-function hoverTimeToHex () {
+function whenHovered () {
+  var theDate = new Date()
+  var seconds = theDate.getSeconds()
+  var hexSeconds = seconds.toString(16)
+  var hex = '#' + hexSeconds + (Math.round((Math.random() * 50) + 1)) + (Math.round((Math.random() * 40) + 1))
   $('#time').html(hex)
-}
-
-function whenTimeisHovered () {
-  hoverTimeToHex()
   clearInterval(clock)
+  clearInterval(changeColor)
 }
 
 function whenNotHovered () {
   $('#time').html(currentTime())
+  var clock = setInterval(currentTime, everySecond)
+  var changeColor = setInterval(changeTimeToHex, everySecond * 2)
 }
 
-$('#time').hover(whenTimeisHovered, whenNotHovered)
+$('#time').hover(whenHovered, whenNotHovered)
