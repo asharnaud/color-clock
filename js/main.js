485ce0013af1147b5d1cdad79522aa0816e2a21e
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-04-07 11:56:52 -0500

    Updated the functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 var $ = window.jQuery
 
+function today () {
+  return new Date().toLocaleTimeString()
+}
 
 function currentTime () {
-  var today = new Date()
-  var time = today.toLocaleTimeString()
-  $('#time').html(time)
+  $('#time').html(today())
 }
 
 currentTime()
@@ -12,11 +13,13 @@ currentTime()
 var everySecond = 1 * 1000
 var clock = setInterval(currentTime, everySecond)
 
+
+var theDate = new Date()
+var seconds = theDate.getSeconds()
+var hexSeconds = seconds.toString(16)
+var hex = '#' + hexSeconds + (Math.round((Math.random() * 50) + 1)) + (Math.round((Math.random() * 40) + 1))
+
 function changeTimeToHex () {
-  var today = new Date()
-  var seconds = today.getSeconds()
-  var hexSeconds = seconds.toString(16)
-  var hex = '#' + hexSeconds + (Math.round((Math.random() * 50) + 1)) + (Math.round((Math.random() * 40) + 1))
   console.log(hex)
   $('body').css('background', hex)
 }
@@ -25,15 +28,12 @@ changeTimeToHex()
 var changeColor = setInterval(changeTimeToHex, everySecond * 2)
 
 function hoverTimeToHex () {
-  var today = new Date()
-  var seconds = today.getSeconds()
-  var hexSeconds = seconds.toString(16)
-  var hex = '#' + hexSeconds + (Math.round((Math.random() * 50) + 1)) + (Math.round((Math.random() * 40) + 1))
   $('#time').html(hex)
 }
 
 function whenTimeisHovered () {
-  hoverTimeToHex ()
+  hoverTimeToHex()
+  clearInterval(clock)
 }
 
 function whenNotHovered () {
