7a13662ba9f9ed7f0dd9a2d4830bcabcc17fd793
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-04-08 09:40:46 -0500

    Added the progress bar and connected it to the seconds.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,64 @@
 var $ = window.jQuery
 
-function today () {
-  return new Date().toLocaleTimeString()
+function getTimeOrHex (input) {
+  var theDate = new Date()
+  var time = new Date().toLocaleTimeString()
+  var seconds = theDate.getSeconds()
+  var hexSeconds = seconds.toString(16)
+  var hex = '#' + hexSeconds + (Math.round((Math.random() * 50) + 1)) + (Math.round((Math.random() * 40) + 1))
+  if (input === 'time') {
+    return time
+  } else if (input === 'seconds') {
+    return seconds
+  } else if (input === 'hex') {
+    return hex
+  }
 }
 
 function currentTime () {
-  $('#time').html(today())
+  $('#time').html(getTimeOrHex('time'))
 }
 
 currentTime()
 
 var everySecond = 1 * 1000
-var clock = setInterval(currentTime, everySecond)
 
-function changeTimeToHex () {
-  var theDate = new Date()
-  var seconds = theDate.getSeconds()
-  var hexSeconds = seconds.toString(16)
-  var hex = '#' + hexSeconds + (Math.round((Math.random() * 50) + 1)) + (Math.round((Math.random() * 40) + 1))
-  console.log(hex)
-  $('body').css('background', hex)
+var clock = setInterval(intervals, everySecond)
+
+function changeBackgroundToHex () {
+  $('body').css('background', getTimeOrHex('hex'))
 }
-changeTimeToHex()
+changeBackgroundToHex()
 
-var changeColor = setInterval(changeTimeToHex, everySecond * 2)
+var changeColor = setInterval(changeBackgroundToHex, everySecond * 2)
+
+function secondsToPercent () {
+  var sec = getTimeOrHex('seconds')
+  console.log(sec)
+  var percent = ((sec * 100) / 60) + '%'
+  console.log(percent)
+  return percent
+}
+
+function fillProgressBar () {
+  $('#progress-bar').css({ width: secondsToPercent() })
+}
+
+function intervals () {
+  fillProgressBar()
+  currentTime()
+}
 
 function whenHovered () {
-  var theDate = new Date()
-  var seconds = theDate.getSeconds()
-  var hexSeconds = seconds.toString(16)
-  var hex = '#' + hexSeconds + (Math.round((Math.random() * 50) + 1)) + (Math.round((Math.random() * 40) + 1))
-  $('#time').html(hex)
+  $('#time').html(getTimeOrHex('hex'))
   clearInterval(clock)
   clearInterval(changeColor)
 }
 
 function whenNotHovered () {
   $('#time').html(currentTime())
-  var clock = setInterval(currentTime, everySecond)
-  var changeColor = setInterval(changeTimeToHex, everySecond * 2)
+  clock = setInterval(intervals, everySecond)
+  changeColor = setInterval(changeBackgroundToHex, everySecond * 2)
 }
 
 $('#time').hover(whenHovered, whenNotHovered)
